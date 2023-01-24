2a05d3cd752ccc288b9300f2a4d4b5bb9c69346f
patience1000 <paulpatience577@gmail.com>
2023-01-24 13:16:40 +0100

    learning component basics

---
+++ Component/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import props from './components/props.vue'
+
+export default{
+  components: {
+    props
+  },
+
+  data(){
+    return{
+      posts:[
+            {id: '1', title: 'How I started'},
+            {id:'2', title:'Vue in View'},
+            {id:'3', title:'How its going'}
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <props v-for="post in posts :key="post.id" :title="post.title"></props>
+</template>
+
