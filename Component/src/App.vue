7dea612dd471876992a03d3c482660416a85b432
patience1000 <paulpatience577@gmail.com>
2023-01-25 11:27:07 +0100

    learning component basics slots

--- Component/src/App.vue
+++ Component/src/App.vue
@@ -1,27 +1,36 @@
 <script>
 import props from './components/props.vue'
-
+import AlertBox from './components/AlertBox.vue'
 export default{
   components: {
-    props
-  },
+    AlertBox
+  }}
 
-  data(){
-    return{
-      posts:[
-            {id: '1', title: 'How I started'},
-            {id:'2', title:'Vue in View'},
-            {id:'3', title:'How its going'}
-      ],
-      postFontSize: 1
-    }
-  }
-}
+//   data(){
+//     return{
+//       posts:[
+//             {id: '1', title: 'How I started'},
+//             {id:'2', title:'Vue in View'},
+//             {id:'3', title:'How its going'}
+//       ],
+//       postFontSize: 1,
+//     }
+//   }
+// }
 </script>
   <template>
-    <button>Enlarge text</button>
-    <div :style="{ fontSize: postFontSize + 'em'}">
-    <props v-for="post in posts" :key="post.id" :title="post.title"></props>
-  </div>
+    <AlertBox>
+      Something bad happened
+    </AlertBox>
+
+
+    <!-- <div :style="{ fontSize: postFontSize + 'em'}">
+    <props 
+    v-for="post in posts" 
+    :key="post.id" 
+    :title="post.title"
+    @enlarge-text="postFontSize += 0.1">
+   </props>
+   </div> -->
   </template>
 
--- Component/src/components/props.vue
+++ Component/src/components/props.vue
@@ -1,9 +1,11 @@
 <script>
 export default {
-    props:[ 'title']
+    props:[ 'title'],
+    emits: ['enlarge-text']
 }
 </script>
 
 <template>
    <h4>{{ title }}</h4> 
+   <button @click="$emit('enlarge-text')">Enlarge text</button>
 </template>
