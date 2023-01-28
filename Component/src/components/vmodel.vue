104c9419b0bfa2d856e59bc532b586bf2693d66b
patience1000 <paulpatience577@gmail.com>
2023-01-28 21:51:14 +0100

    learning v-model components

--- Component/src/components/vmodel.vue
+++ Component/src/components/vmodel.vue
@@ -1,8 +1,19 @@
 <script>
 export default {
     props: ['modelValue'],
-    emits: ['update:modelValue']
+    emits: ['update:modelValue'],
+    computed: {
+        value: {
+            get(value){
+                return this.modelValue
+            },
+            set(){
+                this.$emit('update:modelValue', value)
+            }
+        }
+    }
 }
+
 </script>
 
 <template>
