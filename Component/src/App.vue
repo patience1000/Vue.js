c8413a94362074eeb67c25b99d9b81a067140881
patience1000 <paulpatience577@gmail.com>
2023-01-30 22:32:39 +0100

    learning component  slots

--- Component/src/App.vue
+++ Component/src/App.vue
@@ -28,8 +28,21 @@
 </script>
  
   <template>
-  <AlertBox></AlertBox>
-  <AlertBox> box1</AlertBox>
+  <AlertBox>
+    <template #header>
+    <h1>Here might be a page title</h1>
+  </template>
+
+  <!-- implicit default slot -->
+  <p>A paragraph for the main content.</p>
+  <p>And another one.</p>
+
+  <template #footer>
+    <p>Here's some contact info</p>
+  </template>
+
+  </AlertBox>
+  <!-- <AlertBox> box1</AlertBox> -->
   <!-- <vmodel v-model="message"/> {{ message }} -->
 
     <!-- <AlertBox>
--- Component/src/components/AlertBox.vue
+++ Component/src/components/AlertBox.vue
@@ -1,11 +1,49 @@
+<script>
+export default{
+  data(){
+    return{
+      x:0,
+      y:0
+    }
+  },
+  methods:{
+    update(e){
+      this.x = e.pagex,
+      this.y = e.pagey
+    }
+  },
+  mounted() {
+    window.addEventListener('mousemove', this.update)
+  },
+  unmounted() {
+    window.removeEventListener('mousemove', this.update)
+  }
+}
+    
+</script>
 <template>
+  <slot :x='x' :y="y"/>
+  <p>{{ x }}</p>
     <div class="card">
         <slot>box-content</slot>
     </div>
+  <div class="container">
+   <header>
+    <h1>Here might be a page title</h1>
+  </header>
+  <main>
+    <p>A paragraph for the main content.</p>
+    <p>And another one.</p>
+  </main>
+  <footer>
+    <p>Here's some contact info</p>
+  </footer>
+ </div>
+
 </template>
 
 <style scoped>
-.alert-box {
+.card {
   color: #666;
   border: 1px solid red;
   border-radius: 4px;
