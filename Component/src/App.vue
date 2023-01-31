4870d1a9729f95d656d3c021a8ec4fdfd001efd5
patience1000 <paulpatience577@gmail.com>
2023-01-31 23:00:16 +0100

    learning composables

--- Component/src/App.vue
+++ Component/src/App.vue
@@ -1,47 +1,8 @@
-<script>
-  import props from './components/props.vue'
-  import AlertBox from './components/AlertBox.vue'
-  import vmodel from './components/vmodel.vue'
-  export default{
-    components: {
-      // vmodel
-      // props,
-      AlertBox
-    },
-    data(){
-      return{
-        message: 'hello'
-      }
-    }
-  }
-  //   data(){
-  //     return{
-  //       posts:[
-  //             {id: '1', title: 'How I started'},
-  //             {id:'2', title:'Vue in View'},
-  //             {id:'3', title:'How its going'}
-  //       ],
-  //       postFontSize: 1,
-  //     }
-  //   }
-  // }
+<script setup>
+import { useMouse } from './mouse.js'
+
+const {x, y } = useMouse
 </script>
- 
-  <template>
-  <AlertBox></AlertBox>
-  
-  <!-- <vmodel v-model="message"/> {{ message }} -->
 
-    <!-- <AlertBox>
-      Something bad happened
-    </AlertBox> -->
-    <!-- <div :style="{ fontSize: postFontSize + 'em'}">
-    <props 
-    v-for="post in posts" 
-    :key="post.id" 
-    :title="post.title"
-    @enlarge-text="postFontSize += 0.1">
-   </props> -->
-   <!-- </div> -->
-  </template>
-
+<template>
+<template/>
